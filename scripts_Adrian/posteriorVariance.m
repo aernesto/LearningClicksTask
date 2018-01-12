408fbf7ad83ee6694399449e53bd44146590b550
adrian <adrian.radillo@gmail.com>
2018-01-11 21:17:22 -0600

    can't get long trial, NaN values

--- scripts_Adrian/posteriorVariance.m
+++ scripts_Adrian/posteriorVariance.m
@@ -5,15 +5,16 @@
 
 %% load stimulus
 clear
-load('../data/ClickTrains_h1_rateHigh38_rateLow2_nTrials10000.mat')
+load('../data/ClickTrains_h1_rateHigh38_rateLow2_nTrials1_LONG.mat')
 % number of distinct trial durations in the data array
-N=length(data);
+N=size(data,1);
 % get single trial from longest trial duration (3 sec)
 [clicksCell, T]=data{N,:};
+%T
 % total number of available trials for this trial duration
 nTrials = length(clicksCell);
 trial = 1; % select first trial for now
-%[lTrain,rTrain]=clicksCell{trial, 1:2};
+[lTrain,rTrain]=clicksCell{trial, 1:2};
 
 
 %% set parameters
@@ -30,9 +31,9 @@ alpha=1;
 beta=1;
 priorState=[.5,.5];
 %% call ODE function
-posttimes=.5:.5:18;
-posttimes(end)=18-2*dt;
-T=18;
+posttimes=1:50;
+posttimes(end)=50-2*dt;
+%T
 tic
 [vars, means]=systemODE(lTrain, rTrain, rateLow, rateHigh, T, gamma_max,...
 posttimes, priorState, alpha, beta, dt);
@@ -40,10 +41,12 @@ toc
 subplot(2,1,1)
 plot(posttimes,means,'LineWidth',3)
 ylabel('posterior mean','FontSize',14)
+xlim([0,50])
 title('posterior mean over h','FontSize',14)
 subplot(2,1,2)
 plot(posttimes,vars,'LineWidth',3);
 xlabel('time','FontSize',14)
 ylabel('posterior var','FontSize',14)
+xlim([0,50])
 ylim([0,max(vars)]);
 title('posterior var over h','FontSize',14)
