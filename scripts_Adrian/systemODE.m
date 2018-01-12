9ec5aa8224630cbed42c34995d6854cf819fc431
adrian <adrian.radillo@gmail.com>
2018-01-11 20:14:28 -0600

    in theory systemODE.m finished. NOT TESTED

--- scripts_Adrian/systemODE.m
+++ scripts_Adrian/systemODE.m
@@ -57,9 +57,18 @@ time=0;
 % set delta jump sizes
 kappa = log(rateHigh/rateLow);
 
+post_var_h=zeros(size(posttimes));
+nposttimes = length(posttimes);
+if nposttimes > 0
+    idnxtposttime = 1;
+    nxtposttime=posttimes(1);
+else
+    nxtposttime=inf;
+end
+
 %Forward Euler
 presclick=false; % if true, means at least one click fell in current time bin
-while time<stimulusLength
+while time<T
     jump=0;
     t_new=time + dt;
     if t_new > lnxt
@@ -95,19 +104,50 @@ while time<stimulusLength
     ym_new_gamma0=ym_old(1)+((alpha-1)*exp(-ym_old(1))-alpha)*dt/(time+beta);
     
         % rest of vectors
-        
-        %%%%%%%%%%%%%%%%%%%%%
-        %%%%%% TAKE UP HERE %%%%%%%%%%%%%%%
-    yp_new=yp_old(2:end)+((alpha-1)*exp(-yp_old(1))-alpha)*dt/(time+beta);
+    yp_prime=-(alpha+gammaValues(2:end))';
+    ym_prime=yp_prime;
+    
+    yp_prime=yp_prime+(alpha-1+gammaValues(2:end)').*...
+        exp(ym_old(1:end-1)-yp_old(2:end))';
+    yp_prime=yp_prime / (time + beta);
+    
+    ym_prime=ym_prime+(alpha-1+gammaValues(2:end)').*...
+        exp(yp_old(1:end-1)-ym_old(2:end))';
+    ym_prime=ym_prime / (time + beta);
     
         % concatenate
-    yp_new = [yp_new_gamma0;yp_new];
-    ym_new = [ym_new_gamma0;ym_new];
+    yp_new = [yp_new_gamma0;dt*yp_prime + yp_old(2:end)];
+    ym_new = [ym_new_gamma0;dt*ym_prime + ym_old(2:end)];
     % add jump 
     yp_new = yp_new + jump;
     ym_new = ym_new + jump;
+    
+    % if report time hit, normalize and output posterior variance
+    if time > nxtposttime
+        %normalization constant
+        K = log(sum(exp(yp_new)+exp(ym_new)));
+        %true posterior
+        xp=yp_new-K;
+        xm=ym_new-K;
+    
+        %posterior variance over h
+        v1=(gammaValues'+alpha)/(time+beta);
+        v2=(gammaValues'+alpha+1)/(time+beta);
+        post_var_h(idnxtposttime)=sum((xp+xm).*v1.*v2)-sum((xp+xm).*v1)^2;
         
+        %update index of next reporting time
+        if idnxtposttime == nposttime
+            nxtposttime = inf;
+        else
+            idnxtposttime = idnxtposttime + 1;
+            nxtposttime = posttimes(idnxtposttime);
+        end
+    end
+    
+    % reinitialize for next iteration
+    yp_old = yp_new;
+    ym_old = ym_new;
     time = t_new;
 end
-ss=sign(evidence);
+ss=post_var_h;
 end
