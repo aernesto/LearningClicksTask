93b5b19b8ca3d5183303594ae8b4c6788ada8f04
adrian <adrian.radillo@gmail.com>
2018-01-23 19:51:52 -0600

    can't find mistake in posterior over CPcount

--- scripts_Adrian/posteriorVariance_stimGen.m
+++ scripts_Adrian/posteriorVariance_stimGen.m
@@ -14,19 +14,19 @@ clear
 %% set parameters
 % recall that h is normalized to 1 
 % click rates in Hz
-rateHigh=7;
-rateLow=0.0001;
+rateHigh=10;
+rateLow=.01;
 % time step for forward Euler, in sec
 dt=1/10000;
 % max allowed change point count
-gamma_max=100;
+gamma_max=10;
 % hyperparameters for Gamma dist over hazard rate
 alpha=1;
 beta=1;
 priorState=[.5,.5];
 
 %trial duration (sec)
-T=30;
+T=3;
 
 %% generate stimulus
 data=genStimBank2(1,1,rateHigh,rateLow,T);
@@ -59,10 +59,28 @@ means=[0,means];
 stdevs=[0,stdevs];
 posttimes=[0,posttimes];
 %plot stimulus with change point times
-subplot(2,1,1)
+subplot(5,1,1)
 plotClickTrains(lTrain,rTrain,cptimes, rateLow, rateHigh)
+xlim([0,T])
 title('stimulus with CP times','FontSize',14)
 %plot posterior mean over gamma
-subplot(2,1,2)
+subplot(5,1,2)
 errorbar(posttimes,means,2*stdevs);
+xlim([0,T])
 title('posterior mean over gamma','FontSize',14)
+%plot posterior pmf at t=0 and first positive reporting time
+subplot(5,1,3)
+massOn0=.99;
+priorGamma=[massOn0,ones(1,gamma_max-1)*(1-massOn0)/gamma_max];
+bar(priorGamma)
+title('posterior at t=0')
+ylabel('probability')
+subplot(5,1,4)
+bar(postGamma(:,1))
+ylabel('probability')
+title(['posterior at t=',num2str(posttimes(2),2)])
+subplot(5,1,5)
+bar(postGamma(:,10))
+xlabel('CP count')
+ylabel('probability')
+title(['posterior at t=',num2str(posttimes(11),2)])
